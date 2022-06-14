f350fd157012265f3bbc1ae6f299da148c73e24a
cqhaisenbao <fanyu9353@gmail.com>
2022-06-14 23:04:43 +0800

    实现函数组件渲染

---
+++ Welcome.jsx
@@ -0,0 +1,7 @@
+import React from "./lib/react";
+
+const Welcome = ({name}) => {
+    return <h1 style={{color: 'blue'}}><span>hello</span>,{name}</h1>
+}
+
+export default Welcome;
--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import React from "./lib/react";
 import ReactDOM from "./lib/reactDom";
+import Welcome from "./Welcome";
 
-const x = React.createElement("div", {
-    className: 'title',
-    style: {
-        color: 'red'
-    }
-}, React.createElement("h1", null, "Hello World"))
+const element = React.createElement(Welcome, {name: "luff"});
+console.log(element);
+// const x = React.createElement("div", {
+//     className: 'title',
+//     style: {
+//         color: 'red'
+//     }
+// }, React.createElement("h1", null, "Hello World"))
 
-ReactDOM.render(x, document.getElementById('app'));
+ReactDOM.render(element, document.getElementById('app'));
